c994e2e0269260745673ea4065714c6bf2533610
gabri <gabri@DESKTOP-I4U4N50>
2023-07-07 21:22:22 -0300

    Adicionar arquivos de projeto.

---
+++ ATIV5-PROGRAMA-SENAC/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite a sua idade:");
+        int idade = int.Parse(Console.ReadLine());
+
+        if (idade < 18)
+        {
+            Console.WriteLine("Você é menor de idade, não pode dirigir");
+        }
+        else if (idade >= 18 && idade < 60)
+        {
+            Console.WriteLine("Você é um adulto.");
+        }
+        else
+        {
+            Console.WriteLine("Você é um idoso.");
+        }
+    }
+}
